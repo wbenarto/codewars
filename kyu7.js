9dac8e013bc897570f1461ac343766ba1e56cb43
w.benarto <w.benarto@gmail.com>
2019-09-08 13:35:46 -0700

    3

--- kyu7.js
+++ kyu7.js
@@ -2,3 +2,20 @@
 // function accum(s) {
 //     return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
 //   }
+
+// --- 2 --- Circle of numbers ---
+// function circleOfNumbers(n, firstNumber) {
+//     //coding and coding..
+    
+//     return (firstNumber + n/2) % n
+//   }
+
+// --- 3 --- Calculate year until hit target population ---
+// function nbYear(p0, percent, aug, p) {
+//     // your code
+//     // loop thru year until hit target population
+//     for (var y=0; p0 < p; y++) 
+//     p0 = p0 * (1 + percent/100) + aug
+//     return y
+// }
+  
