d5efa0201004ae34aa3e67142ba986d0353c376f
w.benarto <w.benarto@gmail.com>
2019-09-16 20:37:06 -0700

    37

--- kyu8.js
+++ kyu8.js
@@ -400,6 +400,11 @@
 //   return position + 2*roll
 // }
 
+// --- 37 --- 
+// function howManyLightsabersDoYouOwn(name) {
+//   return (name == 'Zach') ? 18 : 0;
+// }
+
 // parseInt() function parse---s a string argument and returns an integer of the specified 
 // Math.floor() function returns the largest integer less than or equal to a given number.
 // Math.max() returns the highest number in the group. 
