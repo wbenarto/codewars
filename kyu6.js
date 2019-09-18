a6e24710bdb1f78791e6334ddce5912f68d4f752
w.benarto <w.benarto@gmail.com>
2019-09-17 20:53:08 -0700

    kyu6

---
+++ kyu6.js
@@ -0,0 +1,29 @@
+// --- 1 --- return Primorial of number --- 
+// function isPrime(num) {
+//   for (var i =2; i<num; i++) {
+//     if(num%i===0){
+//       return false
+//     }
+//   }
+//   return true
+// }
+
+// function numPrimorial(n){
+//   //your code here
+// //   let result = 0
+// //   for (var i=2; i<n ; i+2) {
+// //     result *= i
+// //   }
+  
+// //   return result
+// let arr = []
+//   let i = 2
+//   while(arr.length < n){
+//     if(isPrime(i)){
+//       arr.push(i)
+//     }
+//    i++
+//   }
+//   return arr.reduce((a,b) => a * b)
+// // return n.map(e => e.reduce((a,b)=> a*b)))
+// }
--- kyu8.js
+++ kyu8.js
@@ -459,6 +459,8 @@
 //   // loop numbers and times 2
 //   return x.map(e => e * 2 )
 // }
+
+
 // parseInt() function parse---s a string argument and returns an integer of the specified 
 // Math.floor() function returns the largest integer less than or equal to a given number.
 // Math.max() returns the highest number in the group. 
