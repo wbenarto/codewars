bdb8ea749f3af2cafe8d815bc5774ff473618236
w.benarto <w.benarto@gmail.com>
2019-10-05 12:27:07 -0700

    53

--- kyu8.js
+++ kyu8.js
@@ -523,6 +523,41 @@
 //   return layers ? 1 + (layers - 1) * Math.sqrt(6) / 3 : 0
 // }
 
+// --- 53 --- print lines ---
+// function HQ9(code) {
+// return (code === "H") ? "Hello World!" 
+//     : (code === "Q") ? "wrong"
+//     : (code = 9) ? "99 bottles of beer on the wall, 99 bottles of beer."+\n+
+// "Take one down and pass it around, 98 bottles of beer on the wall."+\n+
+// "98 bottles of beer on the wall, 98 bottles of beer."+\n+
+// "Take one down and pass it around, 97 bottles of beer on the wall."+\n+
+// "97 bottles of beer on the wall, 97 bottles of beer."+\n+
+// "Take one down and pass it around, 96 bottles of beer on the wall.";
+// }
+
+// // function HQ9(code) {
+// //   switch (code) {
+// //     case 'H': return 'Hello World!';
+// //     case 'Q': return code;
+// //     case '9': return beer(99);
+// //     default: return undefined;
+// //   }
+// // }
+
+// // function beer(count) {
+// //   if (count === 2) {
+// //     return '2 bottles of beer on the wall, 2 bottles of beer.\n' +
+// //         'Take one down and pass it around, 1 bottle of beer on the wall.\n' +
+// //         '1 bottle of beer on the wall, 1 bottle of beer.\n' +
+// //         'Take one down and pass it around, no more bottles of beer on the wall.\n' +
+// //         'No more bottles of beer on the wall, no more bottles of beer.\n' +
+// //         'Go to the store and buy some more, 99 bottles of beer on the wall.';
+// //   } else {
+// //     return `${count} bottles of beer on the wall, ${count} bottles of beer.\n` +
+// //         `Take one down and pass it around, ${count - 1} bottles of beer on the wall.\n` +
+// //         beer(count - 1);
+// //   }
+// // }
 // parseInt() function parse---s a string argument and returns an integer of the specified 
 // Math.floor() function returns the largest integer less than or equal to a given number.
 // Math.max() returns the highest number in the group. 
