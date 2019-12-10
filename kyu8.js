fa4ee69d21db6faaaf9b82ef7cdafa766dda29e3
w.benarto <w.benarto@gmail.com>
2019-12-10 15:20:42 -0800

    63

--- kyu8.js
+++ kyu8.js
@@ -634,6 +634,12 @@
 //   return 'DaniQ'.length
 // }
 
+// --- 63 --- Boolean, function ---
+// function _if(bool, func1, func2) {
+//   // ...
+//   return bool ? func1() : func2()
+// }
+
 
 // parseInt() function parse---s a string argument and returns an integer of the specified 
 // Math.floor() function returns the largest integer less than or equal to a given number.
