462b7129d0edf58a16148bb2e4141091f68a58c7
w.benarto <w.benarto@gmail.com>
2020-05-05 09:27:31 -0700

    69

--- kyu8.js
+++ kyu8.js
@@ -755,6 +755,40 @@
 //   return (distanceToPump <= mpg*fuelLeft) ? 1 : 0
 // };
 
+--- 69 --- Switch Case ---
+function getPlanetName(id){
+  let name;
+  switch(id){
+    case 1:
+      return name = 'Mercury';
+      break;
+    case 2:
+      return name = 'Venus';
+      break;
+    case 3:
+      return name = 'Earth';
+      break;
+    case 4:
+      return name = 'Mars';
+      break;
+    case 5:
+      return name = 'Jupiter';
+      break;
+    case 6:
+      return name = 'Saturn';
+      break;
+    case 7:
+      return name = 'Uranus';
+      break;
+    case 8:
+      return name = 'Neptune';
+      break;
+    default:
+      console.log('nothing');
+      
+  }
+}
+
 
 // parseInt() function parse---s a string argument and returns an integer of the specified 
 // Math.floor() function returns the largest integer less than or equal to a given number.
