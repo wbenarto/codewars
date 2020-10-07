5e39d7d9a7c15b6b0c0003675f32220989f02974
w.benarto <w.benarto@gmail.com>
2020-10-07 13:00:38 -0700

    104

--- kyu8.js
+++ kyu8.js
@@ -1325,6 +1325,12 @@ function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){
   return myTime < sharkTime ? "Alive!" : "Shark Bait!"
 }
 
+--- 104 ---
+function checkAlive(health) {
+  return health > 0;
+}
+
+
 // parseInt() function parse---s a string argument and returns an integer of the specified 
 // Math.floor() function returns the largest integer less than or equal to a given number.
 // Math.max() returns the highest number in the group. 
