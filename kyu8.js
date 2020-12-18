970a8c10806f63b62a3706202f6e401b1b5a3b56
w.benarto <w.benarto@gmail.com>
2020-12-18 10:25:07 -0800

    109

--- kyu8.js
+++ kyu8.js
@@ -1408,6 +1408,22 @@ const quarterOf = (month, quarter = 4) => {
   return quarter;
 };
 
+
+--- 109 ---
+function multiTable(number) {
+  // good luck
+  console.log(number)
+  let result = 1
+  let arr = []
+  for (let i=1; i<=10; i++) {
+    result = number * i
+    arr.push(`${i} * ${number} = ${result}`)
+  }
+  
+  console.log(arr.join('\n'))
+  return arr.join('\n')
+}
+
 // parseInt() function parse---s a string argument and returns an integer of the specified 
 // Math.floor() function returns the largest integer less than or equal to a given number.
 // Math.max() returns the highest number in the group. 
