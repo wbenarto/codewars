24e08bc4da41274ccf5fbd4a177450c906ed27b5
w.benarto <w.benarto@gmail.com>
2021-02-23 13:01:42 -0800

    118

--- kyu8.js
+++ kyu8.js
@@ -1535,6 +1535,10 @@ function dBScale(intensity) {
     }
     return res
   }
+
+  --- 118 ---
+  // Write here your multiply-function
+const multiply = (a,b) => a*b
 // parseInt() function parse---s a string argument and returns an integer of the specified 
 // Math.floor() function returns the largest integer less than or equal to a given number.
 // Math.max() returns the highest number in the group. 
