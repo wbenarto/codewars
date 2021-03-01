9d3e1b1548102f62c636ed67f5738c77c8c324dd
w.benarto <w.benarto@gmail.com>
2021-03-01 09:53:42 -0800

    121

--- kyu8.js
+++ kyu8.js
@@ -1548,6 +1548,13 @@ function aspectRatio(x,y){
   console.log(x,y)
 return [Math.ceil(y*(16/9)), y]
 }
+
+
+121 
+function booleanToString(b){
+  //your code here
+  return String(b)
+}
 // parseInt() function parse---s a string argument and returns an integer of the specified 
 // Math.floor() function returns the largest integer less than or equal to a given number.
 // Math.max() returns the highest number in the group. 
