f0589bcc0567419c64fc8199c2198d8a4b92b586
w.benarto <w.benarto@gmail.com>
2021-03-19 11:43:49 -0700

    2

--- 8kyuPy.py
+++ 8kyuPy.py
@@ -1,7 +1,20 @@
 *** first python codee yawww ** *
 
+# 1
+
 
 def twice_as_old(dad_years_old, son_years_old):
     diff = dad_years_old - son_years_old
 
     return abs(dad_years_old - 2*diff)
+
+
+# 2
+def merge_arrays(arr1, arr2):
+    print(arr1, arr2)
+    res = []
+    p1 = 0
+    p2 = 0
+
+    return sorted(set(arr1+arr2))
+    pass
