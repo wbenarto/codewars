1bf5de70ade03ffa9a10abc8a8cbf6504032cceb
w.benarto <w.benarto@gmail.com>
2021-06-07 11:50:21 -0700

    python rules

--- leetcode_easy.py
+++ leetcode_easy.py
@@ -20,3 +20,52 @@ class Solution:
         c = int(''.join(target))
 
         return c == a+b
+
+
+class Solution:
+    def isSumEqual(self, firstWord: str, secondWord: str, targetWord: str) -> bool:
+        c_offset = ord('a')
+        fw_n = int(''.join([str(ord(c) - c_offset) for c in firstWord]))
+        sw_n = int(''.join([str(ord(c) - c_offset) for c in secondWord]))
+        tw_n = int(''.join([str(ord(c) - c_offset) for c in targetWord]))
+        return fw_n + sw_n == tw_n
+
+
+def isSumEqual(self, firstWord: str, secondWord: str, targetWord: str) -> bool:
+    def val_word(word): return int(
+        "".join(map(str, [abs(ord(c) - 97) for c in word])))
+    return val_word(firstWord) + val_word(secondWord) == val_word(targetWord)
+
+# 2
+
+
+class Solution:
+
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        def sumPart(arr):
+            return sum(arr)
+        result = []
+        for i in accounts:
+            result.append(sumPart(i))
+
+        return sorted(result)[int(len(accounts))-1]
+
+
+class Solution:
+
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        def sumPart(arr):
+            return sum(arr)
+        result = [sumPart(i) for i in accounts]
+
+        return max(result)
+
+# 3
+
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        result = [0]
+        for index, val in enumerate(nums):
+            result.append(result[index] + val)
+        return result[1:]
