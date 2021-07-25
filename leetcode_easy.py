be474f31691a69aaa0c26d735e2c457be45cb59a
w.benarto <w.benarto@gmail.com>
2021-07-25 15:20:54 -0700

    roman to int

--- leetcode_easy.py
+++ leetcode_easy.py
@@ -293,3 +293,20 @@ class Solution(object):
         if first == last[::-1]:
             return True
         return False
+
+
+# 15 Roman to Integer
+class Solution(object):
+    def romanToInt(self, s):
+        dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500,
+                "M": 1000, 'IV': 4, "IX": 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
+        res = 0
+        i = 0
+        while i < len(s):
+            if i+1 < len(s) and s[i:i+2] in dict:
+                res += dict[s[i:i+2]]
+                i += 2
+            else:
+                res += dict[s[i]]
+                i += 1
+        return res
