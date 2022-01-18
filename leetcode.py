276e729175a10424945f91f9ee738d89033bbda2
w.benarto <w.benarto@gmail.com>
2022-01-18 09:06:55 -0800

    #4 Longest Palindrom Substring, use middle and traverse left and right

--- leetcode.py
+++ leetcode.py
@@ -12,8 +12,38 @@ class Solution:
             else:
                 res[e] = i
 
+# 4 <Medium> Longest Palindrome Substring
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res = ''
+        resLen = 0
+
+        for i in range(len(s)):
+            # odd
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l:r+1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+
+            # even
+            l, r = i, i+1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r-l + 1) > resLen:
+                    res = s[l:r+1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+
+        return res
 
 # 4 <Hard> Median of two sorted arrays
+
+
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
 
