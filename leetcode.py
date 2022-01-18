a17ad60c11dcea0bf66194875ff96040532d87a9
w.benarto <w.benarto@gmail.com>
2022-01-18 10:47:33 -0800

    traverse thru list up and down indx

--- leetcode.py
+++ leetcode.py
@@ -12,7 +12,24 @@ class Solution:
             else:
                 res[e] = i
 
-# 4 <Medium> Longest Palindrome Substring
+
+# 4 <Hard> Median of two sorted arrays
+
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+
+        jointList = sorted(nums1 + nums2)
+
+        if len(jointList) % 2 == 1:
+            midIdx = int(len(jointList) / 2)
+            return(jointList[midIdx])
+        else:
+            midIdx = int(len(jointList) / 2)
+            med = (jointList[midIdx-1] + jointList[midIdx]) / 2
+            return(med)
+
+# 5 <Medium> Longest Palindrome Substring
 
 
 class Solution:
@@ -41,18 +58,26 @@ class Solution:
 
         return res
 
-# 4 <Hard> Median of two sorted arrays
+# 6 <Medium> ZigZag Conversion
 
 
 class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+    def convert(self, s: str, numRows: int) -> str:
 
-        jointList = sorted(nums1 + nums2)
+        if numRows == 1 or numRows > len(s):
+            return s
 
-        if len(jointList) % 2 == 1:
-            midIdx = int(len(jointList) / 2)
-            return(jointList[midIdx])
-        else:
-            midIdx = int(len(jointList) / 2)
-            med = (jointList[midIdx-1] + jointList[midIdx]) / 2
-            return(med)
+        res = [[] for i in range(numRows)]
+        idx = 0
+        delta = -1
+        for i in s:
+            res[idx].append(i)
+
+            if idx == 0 or idx == numRows - 1:
+                delta *= -1
+            idx += delta
+
+        for i in range(len(res)):
+            res[i] = ''.join(res[i])
+
+        return(''.join(res))
