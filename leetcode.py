3aeb5dbf9c7b791fbe8f6dde77241068c51abc64
w.benarto <w.benarto@gmail.com>
2022-01-20 10:40:41 -0800

    Linked list add 2 nums, O(n) solution

--- leetcode.py
+++ leetcode.py
@@ -13,6 +13,44 @@ class Solution:
                 res[e] = i
 
 
+# 2 <Medium> Add 2 numbers
+# Definition for singly-linked list.
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+
+        carryover = 0
+        res = ListNode()
+        dummy = res
+
+        #O(n + m)
+        while l1 or l2 or carryover:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            tempVal = v1 + v2 + carryover
+
+            if (tempVal) > 9:
+                tempVal -= 10
+                carryover = 1
+            else:
+                carryover = 0
+
+            dummy.next = ListNode(tempVal)
+            dummy = dummy.next
+
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return(res.next)
+
+
 # 4 <Hard> Median of two sorted arrays
 
 
@@ -59,6 +97,7 @@ class Solution:
         return res
 
 # 6 <Medium> ZigZag Conversion
+# Time = O(n) Space = O(n)
 
 
 class Solution:
