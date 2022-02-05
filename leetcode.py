ccfc1ed329f81687b12dd826f0621ff04e03c8d6
w.benarto <w.benarto@gmail.com>
2022-02-04 16:53:46 -0800

    max sub array 53

--- leetcode.py
+++ leetcode.py
@@ -221,6 +221,23 @@ class Solution:
 
         return dummy.next
 
+# 53 <ez> Max Sub Array
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSub = nums[0]
+        curSum = 0
+
+        for n in nums:
+            if curSum < 0:
+                curSum = 0
+            curSum += n
+            maxSub = max(maxSub, curSum)
+
+        return maxSub
+
+
 # 121 <ez> Sell for max profit
 
 
