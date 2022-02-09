04831904c17735ab67d3b0dfb7a2cb1ed38a33b1
w.benarto <w.benarto@gmail.com>
2022-02-09 10:04:37 -0800

    print tree ez

--- hacker.py
+++ hacker.py
@@ -13,3 +13,10 @@ def compareTriplets(a, b):
             continue
         i += 1
     return [alice, bob]
+
+
+def staircase(n):
+    # Write your code here
+
+    for i in range(1, n+1):
+        print(' '*(n-i) + '#'*i)
