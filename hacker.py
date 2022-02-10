be8a293d0bfcef960f5814d499a3b3996fb86d20
w.benarto <w.benarto@gmail.com>
2022-02-10 08:29:46 -0800

    sliding window on chocolate bar sum

--- hacker.py
+++ hacker.py
@@ -84,3 +84,10 @@ def countApplesAndOranges(s, t, a, b, apples, oranges):
 
     print(numApples)
     print(numOranges)
+
+
+def birthday(s, d, m):
+    # Write your code here
+
+    tp = len(s)-m + 1
+    return len([1 for i in range(tp) if sum(s[i:i+m]) == d])
