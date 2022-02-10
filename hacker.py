4fc7d031cf102ffe9e12cbd1ed270ead299fde06
w.benarto <w.benarto@gmail.com>
2022-02-10 09:44:17 -0800

    pairs socks set

--- hacker.py
+++ hacker.py
@@ -91,3 +91,13 @@ def birthday(s, d, m):
 
     tp = len(s)-m + 1
     return len([1 for i in range(tp) if sum(s[i:i+m]) == d])
+
+
+def sockMerchant(n, ar):
+    # Write your code here
+    pairs = 0
+
+    for e in set(ar):
+        pairs += int(ar.count(e) / 2)
+
+    return pairs
