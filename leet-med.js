fd179d3cb4372bedf5348345d81124683ee6a5ce
w.benarto <w.benarto@gmail.com>
2022-04-02 13:13:08 -0700

    node branch sum recursive

--- leet-med.js
+++ leet-med.js
@@ -1207,3 +1207,26 @@ class BinaryTree {
 
 // Do not edit the line below.
 exports.nodeDepths = nodeDepths;
+
+
+function nodeDepths(root,level=0) {
+  // Write your code here.
+  // dfs, add sum for each node, when at the end, sum 
+  // if node visited
+  if (!root) return 0
+
+  return level + nodeDepths(root.left, level + 1) + nodeDepths(root.right, level + 1)
+  
+}
+
+// This is the class of the input binary tree.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// Do not edit the line below.
+exports.nodeDepths = nodeDepths;
