37269d8df2bdfe2d15eda8ecf26fd91880196e36
w.benarto <w.benarto@gmail.com>
2022-04-06 19:41:03 -0700

    217 O(n)

--- leetcode_easy.py
+++ leetcode_easy.py
@@ -62,6 +62,19 @@ class Solution:
                 profit = prices[i] - minPrice
 
         return profit
+
+
+# 217 find duplicates
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dict = {}
+
+        for i in nums:
+            if i in dict:
+                return True
+            else:
+                dict[i] = 1
+        return False
 # # 1
 # import functools
 
