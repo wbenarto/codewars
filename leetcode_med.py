41aaad2c9cfd003720ef1410d7de79cb4f504690
w.benarto <w.benarto@gmail.com>
2022-04-18 15:26:02 -0700

    O(logn) find target in rotated sorted arr

--- leetcode_med.py
+++ leetcode_med.py
@@ -268,6 +268,33 @@ class Solution:
         
         return res
 
+# 33 Find target in rotated sorted array
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l , r = 0, len(nums) - 1
+        
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target: 
+                return m
+            
+            # left sorted
+            if nums[l] <= nums[m]:
+                if target < nums[l] or target > nums[m]:
+                    l = m + 1
+                else: 
+                    r = m - 1
+            # right sorted
+            else: 
+                if target < nums[m] or target > nums[r]:
+                    r = m - 1
+                else: 
+                    l = m + 1
+    
+            
+        return -1
+
+
 # 152 Max product subarray
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
