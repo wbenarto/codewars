de596e62fe73c08e52ee7bd8bf4dbfb8122b61c7
w.benarto <w.benarto@gmail.com>
2022-04-18 16:35:26 -0700

    O(n^2) using pointers

--- leetcode_med.py
+++ leetcode_med.py
@@ -294,6 +294,32 @@ class Solution:
             
         return -1
 
+# 15 3sum
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        
+        for i, e in enumerate(nums):
+            if i > 0 and e == nums[i-1]:
+                continue
+            
+            l , r = i + 1, len(nums) - 1
+            
+            while l < r:
+                threeSum = e + nums[l] + nums[r]
+                
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0: 
+                    l += 1
+                else:
+                    res.append([e, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l-1] and l < r:
+                        l += 1
+        return res
+
 
 # 152 Max product subarray
 class Solution:
