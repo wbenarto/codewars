147bc3e965ae4c9da5ddd3a65bf7611ecc0f6b38
w.benarto <w.benarto@gmail.com>
2022-04-18 16:43:12 -0700

    max Area of water O(n)

--- leetcode_med.py
+++ leetcode_med.py
@@ -238,6 +238,23 @@ class Solution:
         
     def calcArea(self, indexWidth, height):
         return indexWidth * height
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        tempMax = 0
+        
+        l , r = 0, len(height) - 1
+        
+        while l < r:
+            dist = r - l
+            area = min(height[l], height[r]) * dist
+            tempMax = max(tempMax, area)
+            
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return tempMax
    
         
     
