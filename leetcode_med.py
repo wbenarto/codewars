1ed999c8b26a9cae44cffce7bba5f2de1ac2a539
w.benarto <w.benarto@gmail.com>
2022-04-21 19:07:10 -0700

    3sum

--- leetcode_med.py
+++ leetcode_med.py
@@ -247,6 +247,28 @@ class Solution:
                 r = m - 1
                 
         return minNum
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        for i, e in enumerate(nums):
+            if i>0 and nums[i] == nums[i-1]:
+                continue
+            
+            l , r = i + 1, len(nums) - 1
+            
+            while l < r:
+                tsome = e + nums[l] + nums[r]
+                if tsome > 0:
+                    r -= 1
+                elif tsome < 0:
+                    l += 1
+                else:
+                    res.append([e, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l-1] and l < r:
+                        l += 1
+        return res
  
  class Solution:
     def search(self, nums: List[int], target: int) -> int:
