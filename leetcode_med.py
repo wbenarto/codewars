50e00a1638e94db6ddf8cf4e38946bd45756eda3
w.benarto <w.benarto@gmail.com>
2022-04-21 18:58:10 -0700

    search in rotated sorted arr

--- leetcode_med.py
+++ leetcode_med.py
@@ -248,6 +248,27 @@ class Solution:
                 
         return minNum
  
+ class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l , r = 0, len(nums) - 1
+        
+        while l<=r:
+            m=(l+r)//2
+            
+            if nums[m] == target:
+                return m
+            
+            if nums[m] >= nums[l]:
+                if target < nums[l] or target > nums[m]:
+                    l = m + 1
+                else:
+                    r = m - 1
+            else:
+                if target > nums[r] or target < nums[m]:
+                    r = m - 1
+                else: 
+                    l = m + 1
+        return -1
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         res = nums[0]
