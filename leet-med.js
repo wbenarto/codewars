ab5596b604316b56440da0310d6e896f7549d234
w.benarto <w.benarto@gmail.com>
2022-05-20 14:55:14 -0700

    invert binary tree recursively

--- leet-med.js
+++ leet-med.js
@@ -1230,3 +1230,29 @@ class BinaryTree {
 
 // Do not edit the line below.
 exports.nodeDepths = nodeDepths;
+
+function invertBinaryTree(tree) {
+  // Write your code here.
+  if (!tree) return null
+  if (!tree.left && !tree.right) return
+
+  let temp = tree.left
+  tree.left = tree.right
+  tree.right = temp
+
+  invertBinaryTree(tree.left)
+  invertBinaryTree(tree.right)
+  return tree
+}
+
+// This is the class of the input binary tree.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// Do not edit the line below.
+exports.invertBinaryTree = invertBinaryTree;
