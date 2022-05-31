7c623e8f3be97f203e4e1e7ec99fbdf56bcaa79d
w.benarto <w.benarto@gmail.com>
2022-05-30 22:16:20 -0700

    longest substring no duplicate

--- leetcode_med.py
+++ leetcode_med.py
@@ -305,7 +305,21 @@ class Solution:
                 r -= 1
                 
             
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l = 0
+        res = 0
+        charSet = set()
         
+        
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])    
+            res = max(res, r- l + 1)
+        return res
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = []
