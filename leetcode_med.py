c819cb940c8dd743ae2e1927a8175f732dc3e6ef
w.benarto <w.benarto@gmail.com>
2022-05-31 16:40:31 -0700

    longest repeating char

--- leetcode_med.py
+++ leetcode_med.py
@@ -564,3 +564,22 @@ class Solution:
             postfix *= nums[i]
          
         return res
+
+
+# 424 longest repeating char
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        dict = {}
+        res = 0
+        l = 0
+        
+        for r in range(len(s)):
+            dict[s[r]] = 1 + dict.get( s[r] , 0)
+            
+            while (r - l + 1) - max(dict.values()) > k:
+                dict[s[l]] -= 1
+                l += 1
+            
+            res = max(res, r-l+1)
+        return res
+        
