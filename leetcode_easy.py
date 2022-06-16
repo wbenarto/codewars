b4c5304c085d1d7afe4ee79b4cf42c90d2cdd4e3
w.benarto <w.benarto@gmail.com>
2022-06-16 10:43:05 -0700

    binary search

--- leetcode_easy.py
+++ leetcode_easy.py
@@ -1464,3 +1464,19 @@ class Solution:
 #             return True
 #         else:
 #             return False
+# Easy binary search
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            if nums[mid] == target:
+                return mid
+            if target < nums[mid]:
+                r = mid - 1
+            elif target > nums[mid]:
+                l = mid + 1
+
+        return -1
