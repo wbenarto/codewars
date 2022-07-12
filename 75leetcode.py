69d3ce9bbcbdd9dbf78a3547c4da5d7ef9a5d54d
w.benarto <w.benarto@gmail.com>
2022-07-12 12:29:54 -0700

    cycle linked list

--- 75leetcode.py
+++ 75leetcode.py
@@ -42,3 +42,38 @@ class Solution:
             
         return dummy.next
     
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        res = {}
+        while head:
+            if head.next in res:
+                return True
+            else:
+                res[head.next] = head.next
+            head = head.next
+        
+        return False
+            
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+        
+        while fast and fast.next:
+            
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast: 
+                return True
+        return False
