2b20eb8f430558eac6083b484091528ecaf0fedb
w.benarto <w.benarto@gmail.com>
2022-09-26 23:31:52 -0700

    focus

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -44,41 +44,7 @@ class BST {
       searchTree(this.root);
     }
   }
-  //   add(data) {
-  //     const node = this.root;
-  //     if (node === null) {
-  //       this.root = new Node(data);
-  //       return;
-  //     } else {
-  //       const searchTree = function (node) {
-  //         if (data < node.data) {
-  //           if (node.left === null) {
-  //             node.left = new Node(data);
-  //             return;
-  //           } else if (node.left !== null) {
-  //             return searchTree(node.left);
-  //           }
-  //         } else if (data > node.data) {
-  //           if (node.right === null) {
-  //             node.right = new Node(data);
-  //             return;
-  //           } else if (node.right !== null) {
-  //             return searchTree(node.right);
-  //           }
-  //         } else {
-  //           return null;
-  //         }
-  //       };
-  //       return searchTree(node);
-  //     }
-  //   }
-  //   findMin() {
-  //     let current = this.root;
-  //     while (current.left !== null) {
-  //       current = current.left;
-  //     }
-  //     return current.data;
-  //   }
+
   findMin() {
     let curMin = this.root;
     while (curMin.left) {
