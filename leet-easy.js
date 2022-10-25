556a2e9aec7157df69d911753f61ea929fa4d9b2
w.benarto <w.benarto@gmail.com>
2022-10-24 23:52:45 -0700

    pivot index

--- leet-easy.js
+++ leet-easy.js
@@ -13,3 +13,30 @@
     
     return res
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var pivotIndex = function(nums) {
+    let l=0
+    let r=nums.length-1
+    console.log(l, r)
+    let leftSum = nums[0]
+    let rightSum = nums[r]
+    
+    while (r - l !== 2) {
+        
+        if (leftSum < rightSum) {
+            l += 1
+            leftSum += nums[l]
+        } else if (leftSum > rightSum) {
+            r -= 1
+            rightSum += nums[r]
+        }
+    }
+    
+    console.log(leftSum, rightSum)
+    if (leftSum == rightSum) return r - 1
+    else return -1
+};
