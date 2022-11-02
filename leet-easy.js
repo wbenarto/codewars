22c658fb7ce79f74860c33f46c0e73a44cbe9d09
w.benarto <w.benarto@gmail.com>
2022-11-01 23:45:07 -0700

    reverse linked list

--- leet-easy.js
+++ leet-easy.js
@@ -153,3 +153,18 @@ var mergeTwoLists = function(list1, list2) {
     
     return head.next
 };
+
+// Reverse linked list
+var reverseList = function(head) {
+    if(head === null) return head;
+    let arr = [];
+    while (head) {
+        arr.push(head)
+        head = head.next;
+    }    
+    arr[0].next = null;
+    for (let i = 1; i < arr.length; i++) {
+        arr[i].next = arr[i-1]
+    }
+    return arr[arr.length-1];
+};
