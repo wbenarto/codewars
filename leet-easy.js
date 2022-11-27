533a90073c8cb78ba586133dade8ecf694241f47
w.benarto <w.benarto@gmail.com>
2022-11-27 09:32:47 -0800

    a leetcode a day keeps unemployment away

--- leet-easy.js
+++ leet-easy.js
@@ -346,3 +346,39 @@ var reverseList = function(head) {
     
     return cur.next
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let j=0
+    for (let i=1; i<nums.length;i++) {
+        if (nums[i] == nums[j]) {
+            nums.splice(i , 1) 
+            i--
+        }
+        else {j++}
+    }
+
+};
+
+var removeDuplicates = function(nums) {
+    if(nums.length === 0) {
+        return 0
+    }
+    let result = 1, i = 0, j = 1;
+    
+    while(i < nums.length && j < nums.length) {
+        if(nums[j] === nums[i]) {
+            j++;
+        } else {
+            result += 1;
+            i++;
+            nums[i] = nums[j];
+            j++;
+        }
+    }
+    
+    return result;
+};
