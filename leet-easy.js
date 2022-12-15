3b55cb21e8ec9af88a8a77002c66c2dc2d451d38
w.benarto <w.benarto@gmail.com>
2022-12-14 21:52:06 -0800

    diameter of tree using dfs

--- leet-easy.js
+++ leet-easy.js
@@ -889,3 +889,32 @@ var rangeSumBST = function(root, low, high) {
     dfs(root, '')
     return sum
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var diameterOfBinaryTree = function(root) {
+    let path = 0
+
+    function dfs(root) {
+        if (root == null ) return 0
+       
+        let l = dfs(root.left)
+        let r = dfs(root.right)
+        path = Math.max( path, l+r+1)
+        return Math.max(l, r)  + 1
+    }
+    dfs(root)
+
+    return path - 1 
+ 
+};
