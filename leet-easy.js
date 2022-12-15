d12677d99bbecdc548e1056b14c20871f9ec3ae9
w.benarto <w.benarto@gmail.com>
2022-12-14 21:04:58 -0800

    tree binary sum traversal

--- leet-easy.js
+++ leet-easy.js
@@ -749,3 +749,143 @@ var rangeSumBST = function(root, low, high) {
 
     
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ var inorderTraversal = function(root) {
+    let res = []
+    dfs(root)
+
+    function dfs(root) {
+        if (root !== null) {
+            dfs(root.left)
+            res.push(root.val)
+            dfs(root.right)
+        }
+    }
+
+    return res
+};
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ var inorderTraversal = function(root) {
+    let res = []
+    dfs(root)
+
+    function dfs(root) {
+        if (root !== null) {
+            dfs(root.left)
+            res.push(root.val)
+            dfs(root.right)
+        }
+    }
+
+    return res
+};
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ var preorderTraversal = function(root) {
+
+    let res = []
+    dfs(root)
+    
+    function dfs(root) {
+        if (root !== null) {
+            res.push(root.val)
+            dfs(root.left)
+            dfs(root.right)
+        }
+    }
+
+    return res
+    
+};
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ var postorderTraversal = function(root) {
+    let res = []
+    dfs(root)
+
+    function dfs(root) {
+        if (root !== null) {
+            dfs(root.left)
+            dfs(root.right)
+            res.push(root.val)
+        }
+    }
+
+    return res
+};
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var sumRootToLeaf = function(root) {
+    let sum = 0
+
+    function dfs(root, binary='') {
+    
+        if (root == null) return 0
+        if (root.left == null && root.right == null) {
+            sum += parseInt(binary+root.val, 2)
+        }
+        dfs(root.left, binary+root.val)
+        dfs(root.right, binary+root.val)
+    }
+
+    dfs(root, '')
+    return sum
+};
