c26eae70c0427b965cabf15f408ff21318fcd02d
w.benarto <w.benarto@gmail.com>
2022-12-15 16:56:41 -0800

    dfs on subtree

--- leet-easy.js
+++ leet-easy.js
@@ -971,3 +971,29 @@ var rangeSumBST = function(root, low, high) {
 
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right) 
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+ var isSubtree = function(root, subRoot) {
+    if (!root) return false
+
+    function isSametree(a, b) {
+        if (!a && !b) return true
+        if (!a || !b || a.val !== b.val) return false
+        return isSametree(a.left, b.left) && isSametree(a.right, b.right)
+    }
+
+    if (isSametree(root, subRoot)) return true
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
+};
