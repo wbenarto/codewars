674633d4b9ada35e8d587bfc68d62b144a08dc52
w.benarto <w.benarto@gmail.com>
2022-12-20 21:04:11 -0800

    construct tree from inorder preorder traversal

--- leet-med.js
+++ leet-med.js
@@ -143,3 +143,36 @@ function ArrayChallenge(arr) {
     return arr; 
   
   }
+
+  /**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function(preorder, inorder) {
+  if (preorder.length == 0 || inorder.length == 0) return null
+
+  return build(preorder, inorder)
+
+  function build(preorder, inorder ){
+       if (preorder.length == 0 || inorder.length == 0) return null
+      const res = new TreeNode(preorder[0])
+      const midIdx = inorder.indexOf(preorder[0])
+      res.left = build(preorder.slice(1,midIdx+1), inorder.slice(0, midIdx))
+      res.right = build(preorder.slice(midIdx+1), inorder.slice(midIdx+1))        
+
+      return res
+  }
+  
+
+
+  
+};
