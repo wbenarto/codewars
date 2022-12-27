b3632508270b43cb2df669fb7a66b5639e693edd
w.benarto <w.benarto@gmail.com>
2022-12-27 08:25:33 -0800

    boolean binary tree

--- leet-easy.js
+++ leet-easy.js
@@ -1100,3 +1100,29 @@ var rangeSumBST = function(root, low, high) {
     
     
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var evaluateTree = function(root) {
+    // check rootleft and rootright, apply the conditions
+
+    if (!root) return
+    if (root.val == 0 || root.val == 1) {
+        return root.val
+    }
+    if (root.val == 2 ) {
+        return evaluateTree(root.left) || evaluateTree(root.right)
+    } else if (root.val == 3) {
+        return evaluateTree(root.left) && evaluateTree(root.right)
+    }
+};
