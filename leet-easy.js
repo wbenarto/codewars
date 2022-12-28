620be3a42a8b287a22f2fa5bcdf87670d7feb199
w.benarto <w.benarto@gmail.com>
2022-12-27 16:57:06 -0800

    one liner dfs unival tree

--- leet-easy.js
+++ leet-easy.js
@@ -1423,3 +1423,21 @@ var increasingBST = function(root) {
 
     
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+ var isUnivalTree = function(root, value=root.val) {
+    if (!root) return true
+
+    return root.val == value && isUnivalTree(root.left, value) && isUnivalTree(root.right, value)
+};
