15ace529b7c99abc2502f47b2e411702139600f6
w.benarto <w.benarto@gmail.com>
2022-12-27 16:36:28 -0800

    BST max depth DFS

--- leet-easy.js
+++ leet-easy.js
@@ -1336,3 +1336,24 @@ var increasingBST = function(root) {
     }
     return depth + 1
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var maxDepth = function(root) {
+    if (!root) return 0
+    let depth = 0
+
+    depth = Math.max(depth, maxDepth(root.left), maxDepth(root.right))
+
+    return depth + 1
+};
