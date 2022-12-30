cfc102522c96f4bdd47924eb0acc811f2692cd9d
w.benarto <w.benarto@gmail.com>
2022-12-29 21:29:48 -0800

    build pascal triangle DP

--- leet-easy.js
+++ leet-easy.js
@@ -1891,3 +1891,41 @@ OrderedStream.prototype.insert = function(idKey, value) {
     console.log([...hash].find(([key, val])=>val==n)[0])
     return [...hash].find(([key, val])=>val==n)[0]
 };
+
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+ var countBits = function(n) {
+    let dp = Array(n+1).fill(0)
+    let offset = 1
+    console.log(dp)
+
+    for (let i=1;i<=n;i++) {
+        if (offset * 2 == i) offset *= 2
+        dp[i] = 1 + dp[i-offset]
+    }
+    console.log(dp)
+    return dp
+    
+};
+
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+ var generate = function(numRows) {
+    let res = [[1]]
+
+    for (let i=1;i<numRows;i++) {
+        let temp = [0, ...res[res.length-1], 0]
+        let row = []
+
+        for (let j=0;j<res.at(-1).length+1; j++) {
+            row.push(temp[j] + temp[j+1])
+        }
+        res.push(row)
+    }
+
+    return res
+};
