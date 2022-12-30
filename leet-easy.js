4d88e6028bfc01b8f6d320d69eb78b90facbd682
w.benarto <w.benarto@gmail.com>
2022-12-29 22:44:59 -0800

    tribonacci DP with memo

--- leet-easy.js
+++ leet-easy.js
@@ -2000,3 +2000,16 @@ var tribonacci = function(n) {
 
     return sum
 };
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var tribonacci = function(n, memo) {
+    memo = memo || {}
+    if (n<2) return n
+    if (n==2) return 1
+    if (memo[n]) return memo[n]
+    
+    return memo[n] = tribonacci(n-1, memo) + tribonacci(n-2,memo) + tribonacci(n-3, memo)
+};
