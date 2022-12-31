3e62865ef7928f91dca3f314282a9440866f8276
w.benarto <w.benarto@gmail.com>
2022-12-31 15:50:15 -0800

    pascal triangle

--- leet-easy.js
+++ leet-easy.js
@@ -2034,3 +2034,25 @@ var tribonacci = function(n) {
     return sum
 
 };
+
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+ var getRow = function(rowIndex) {
+
+    let res = [[1]]
+
+    for (let i=0;i<rowIndex;i++){
+        let temp = [0, ...res[res.length-1], 0]
+        let row = []
+        for (let j=0;j<temp.length-1;j++) {
+
+            row.push(temp[j] + temp[j+1])
+        }
+        res.push(row)
+    }
+
+    return res[res.length-1]
+
+};
