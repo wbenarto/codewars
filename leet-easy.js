f9a6c06b5c8e311ad7d41629957734c4124a8c8b
w.benarto <w.benarto@gmail.com>
2023-01-02 22:35:06 -0800

    queue implementation using stack

--- leet-easy.js
+++ leet-easy.js
@@ -2098,3 +2098,46 @@ var makeGood = function(s) {
 
     return stack.join('')
 };
+
+var MyQueue = function() {
+    this.stack = []
+   
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.stack.push(x)
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    return this.stack.shift()
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    return this.stack[0]
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return this.stack.length == 0 ? 1 : 0
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
