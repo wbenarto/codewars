e57a9648e91f91e8206ae55e5c5e44d8385d9779
w.benarto <w.benarto@gmail.com>
2023-01-03 13:23:09 -0800

    students sandwiches

--- leet-easy.js
+++ leet-easy.js
@@ -2261,3 +2261,19 @@ var removeOuterParentheses = function(s) {
     }
     return stack.reduce((a,b)=>a+b, 0)
 };
+
+/**
+ * @param {number[]} students
+ * @param {number[]} sandwiches
+ * @return {number}
+ */
+ var countStudents = function(students, sandwiches) {
+    while (students.length > 0 && students.indexOf(sandwiches[0]) != -1) {
+        if (students[0] == sandwiches[0]) {
+            students.shift()
+            sandwiches.shift()
+        }
+        else students.push(students.shift())
+    }
+    return students.length
+};
