9ff9f7cccfe53efdb6498847c7ebb78a912b7edd
w.benarto <w.benarto@gmail.com>
2023-01-02 21:12:33 -0800

    valid parentheses

--- leet-easy.js
+++ leet-easy.js
@@ -2056,3 +2056,28 @@ var tribonacci = function(n) {
     return res[res.length-1]
 
 };
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ var isValid = function(s) {
+    let stack = []
+    let map = {
+        '{' : '}',
+        '[' : ']',
+        '(' : ')'
+    }
+ 
+     for (let i=0; i<s.length;i++) {
+         if (s[i] == '(' || s[i] == '[' || s[i] == '{' ) {
+             stack.push(s[i])
+         } else if (map[stack.pop()] !== s[i]) {
+             return false
+         }
+     }
+ 
+     return stack.length ? 0 : 1
+  
+    
+ };
