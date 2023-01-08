44845aa320ab2fcaf9de7433b31de6a02747452a
w.benarto <w.benarto@gmail.com>
2023-01-07 21:20:40 -0800

    med leet gas station

--- leet-med.js
+++ leet-med.js
@@ -428,3 +428,19 @@ var generateParenthesis = function(n) {
   }
   return res
 };
+
+var canCompleteCircuit = function(gas, cost) {
+  let totalTank = 0;
+  let currentTank = 0;
+  let startingStation = 0;
+  for (let i = 0; i < gas.length; i++) {
+      const netCost = gas[i] - cost[i];
+      totalTank += netCost;
+      currentTank += netCost;
+      if (currentTank < 0) {
+          startingStation = i+1;
+          currentTank = 0;
+      }
+  }
+  return totalTank < 0 ? -1 : startingStation;
+};
