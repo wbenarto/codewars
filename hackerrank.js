202cf77b2228e4071fb564d0c188c5f0a4a2c748
w.benarto <w.benarto@gmail.com>
2023-01-09 21:36:55 -0800

    hacker rank interview2

--- hackerrank.js
+++ hackerrank.js
@@ -74,3 +74,72 @@ function main() {
 
     ws.end();
 }
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+/*
+ * Complete the 'isPossible' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER a
+ *  2. INTEGER b
+ *  3. INTEGER c
+ *  4. INTEGER d
+ */
+
+function isPossible(a, b, c, d) {
+    // Write your code here
+    while (a <= c && b <= d) {
+
+        if (a==c && b==d) return "Yes"
+        
+        a = a+b
+        b = a+b
+    }
+    
+    return "No"
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const a = parseInt(readLine().trim(), 10);
+
+    const b = parseInt(readLine().trim(), 10);
+
+    const c = parseInt(readLine().trim(), 10);
+
+    const d = parseInt(readLine().trim(), 10);
+
+    const result = isPossible(a, b, c, d);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
