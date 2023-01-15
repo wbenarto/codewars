3c2ebe20f0aa8ab058ce39e62ebba79f8b35d498
w.benarto <w.benarto@gmail.com>
2023-01-15 09:59:55 -0800

    merge sorted array

--- leet-easy.js
+++ leet-easy.js
@@ -2562,3 +2562,19 @@ var getIntersectionNode = function(headA, headB) {
     console.log(newDigits)
     return newDigits
 };
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+ var merge = function(nums1, m, nums2, n) {
+    let insertPos = m + n - 1
+    m--
+    n--
+    while (n>=0) {
+        nums1[insertPos--] = (nums1[m] > nums2[n]) ? nums1[m--] : nums2[n--]
+    }
+};
