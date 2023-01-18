b33a5ad402a9523150c5c60668393c8a4d635e66
w.benarto <w.benarto@gmail.com>
2023-01-17 20:38:58 -0800

    min moves to seat

--- leet-easy.js
+++ leet-easy.js
@@ -2829,3 +2829,19 @@ var firstPalindrome = function(words) {
       }
       return true;
     }
+
+/**
+ * @param {number[]} seats
+ * @param {number[]} students
+ * @return {number}
+ */
+ var minMovesToSeat = function(seats, students) {
+    let res = 0
+    seats.sort((a,b)=> a-b)
+    students.sort((a,b)=> a-b)
+    for (let i=0;i<seats.length;i++) {
+        res += Math.abs(seats[i] - students[i]) 
+    }
+
+    return res
+};
