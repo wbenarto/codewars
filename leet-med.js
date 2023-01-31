810efab2eca16a2b4ec8e0871e20434316b894c9
w.benarto <w.benarto@gmail.com>
2023-01-30 22:35:23 -0800

    max subset no adjacent, dynamic programming O(n) time O(1) space

--- leet-med.js
+++ leet-med.js
@@ -663,3 +663,23 @@ function cycleInGraph(edges) {
 
 // Do not edit the line below.
 exports.cycleInGraph = cycleInGraph;
+
+
+function maxSubsetSumNoAdjacent(array) {
+  // Write your code here.
+  if (array.length == 0) return 0
+  else if (array.length == 1) return array[0]
+
+  let second = array[0]
+  let first = Math.max(array[0], array[1])
+
+  for (let i=2; i<array.length;i++){
+    let cur = Math.max(first, second+array[i])
+    second = first
+    first = cur
+  }
+  return first
+}
+
+// Do not edit the line below.
+exports.maxSubsetSumNoAdjacent = maxSubsetSumNoAdjacent;
